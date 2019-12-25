af74afcf67ba55da7ab910ab2c4ebc1f70b83bc9
Debra Ray <raydebra89@gmail.com>
2019-12-24 19:16:47 -0500

    update

---
+++ Session8_DataCamp_Lesson/regression_tutorial.r
@@ -0,0 +1,29 @@
+#https://www.datacamp.com/community/tutorials/linear-regression-R
+
+library(readxl)
+ageandheight <- read_excel("ageandheight.xls", sheet = "Hoja2") #Upload the data
+lmHeight = lm(height~age, data = ageandheight) #Create the linear regression
+summary(lmHeight) #Review the results
+lmHeight2 = lm(height~age + no_siblings, data = ageandheight) #Create a linear regression with two variables
+summary(lmHeight2) #Review the results
+
+pressure <- read_excel("pressure.xlsx") #Upload the data
+lmTemp = lm(Pressure~Temperature, data = pressure) #Create the linear regression
+plot(pressure, pch = 16, col = "blue") #Plot the results
+abline(lmTemp) #Add a regression line
+
+summary(lmTemp)
+
+plot(lmTemp$residuals, pch = 16, col = "red")
+
+lmTemp2 = lm(Pressure~Temperature + I(Temperature^2), data = pressure) #Create a linear regression with a quadratic coefficient
+summary(lmTemp2) #Review the results
+
+plot(lmTemp2$residuals, pch = 16, col = "red")
+
+ageandheight[2, 2] = 7.7
+head(ageandheight)
+
+lmHeight3 = lm(height~age, data = ageandheight)#Create the linear regression
+summary(lmHeight3)#Review the results
+plot(cooks.distance(lmHeight3), pch = 16, col = "blue") #Plot the Cooks Distances.
